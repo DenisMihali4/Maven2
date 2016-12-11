5265966ca145aeddc5e7189fa5bbfbde856bf6b8
HillelTeacher <HillelTeacher>
2016-12-11 15:05:15 +0200

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,10 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class main {
+    private final static Logger LL = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args){
+        LL.warn("Helo wollf");
+    }
+
+}
